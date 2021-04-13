5cb6b2f36f9f1b680e316c147c6e4ec745e40efc
Haydn Brown <haydnpbrown1@gmail.com>
2021-04-13 19:01:48 -0400

    initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <sys/sem.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
