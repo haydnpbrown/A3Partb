df3f8e27ac3a5fe959eadc79c6dc0fab4d5f63d5
chasefridgen <55394210+chasefridgen@users.noreply.github.com>
2021-04-13 22:02:20 -0400

    final DB_editor push

--- DB_editor.c
+++ DB_editor.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/msg.h>
+#include <unistd.h>
 #include "struct_types.h"
 
 #define MAX_TEXT 100000
@@ -38,13 +39,8 @@ int main(){
 
         current_acc.funds = (float) strtod(tempFunds, NULL);
 
-//        printf("Summary of entered values\n");
-//        printf("Account #: %s\n", current_acc.acc_num);
-//        printf("Pin: %s\n", current_acc.pin);
-//        printf("Balance: %f\n", current_acc.funds);
-
-
-        msg.message_type = 0;
+        
+        msg.message_type = 1;
         msg.msg_type = "UPDATE_DB";
         msg.contents = current_acc;
 
@@ -52,6 +48,8 @@ int main(){
             printf("error sending message");
             exit(EXIT_FAILURE);
         }
+
+        printf("Account added to Database\\n");
     }
 }
 
